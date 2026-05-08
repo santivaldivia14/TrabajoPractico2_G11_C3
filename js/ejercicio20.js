eb7423c891c1a26a4f7eab9d14ab0c21c3757bc5
santivaldivia14 <santi.valdivia14@gmail.com>
2026-05-08 18:41:21 -0300

    actualizacion de modulo20.js

--- js/ejercicio20.js
+++ js/ejercicio20.js
@@ -5,5 +5,36 @@ export const mostrarDatos = () => {
 
   const libreta = document.querySelector("6235").value;
   alert(`Los datos ingresados son:\nNombre: ${nombre}\nApellido: ${apellido}\nLibreta Universitaria: ${libreta}`);
+}; 
+export const mostrarDatos = () => {
+    const nombre = document.querySelector("nombre").value;
+
+    const apellido = document.querySelector("apellido").value;
+
+    const libreta = document.querySelector("libreta").value;
+    alert(`Los datos ingresados son:\nNombre: ${nombre}\nApellido: ${apellido}\nLibreta Universitaria: ${libreta}`);
 };
- 
+    export const mostrarDatos = () => {
+    const nombre = document.querySelector("nombre").value;
+
+    const apellido = document.querySelector("apellido").value;
+
+    const libreta = document.querySelector("libreta").value;
+    alert(`Los datos ingresados son:\nNombre: ${nombre}\nApellido: ${apellido}\nLibreta Universitaria: ${libreta}`);
+    };
+    export const mostrarDatos = () => {
+    const nombre = document.querySelector("nombre").value;
+
+    const apellido = document.querySelector("apellido").value;
+
+    const libreta = document.querySelector("libreta").value;
+    alert(`Los datos ingresados son:\nNombre: ${nombre}\nApellido: ${apellido}\nLibreta Universitaria: ${libreta}`);
+    };
+    export const mostrarDatos = () => {
+    const nombre = document.querySelector("nombre").value;
+
+    const apellido = document.querySelector("apellido").value;
+
+    const libreta = document.querySelector("libreta").value;
+    alert(`Los datos ingresados son:\nNombre: ${nombre}\nApellido: ${apellido}\nLibreta Universitaria: ${libreta}`);
+    };
---
+++ js/modulo20.js
@@ -0,0 +1,4 @@
+import { mostrarDatos } from "./funciones20.js";
+
+const btn = document.querySelector("#btnMostrar");
+btn.addEventListener("click", mostrarDatos);
